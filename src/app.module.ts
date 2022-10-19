86ca4830ba978d454cea2a35df68a3c45be82360
RODRIGO CUEVA PASTOR <chaboxx159@gmail.com>
2022-10-18 21:16:12 -0500

    feat : federation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,25 @@ import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { PrismaService } from "./prisma.service";
 import { join } from "path";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from "@nestjs/apollo";
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import { GeneralParameterModule } from "./general-parameter/general-parameter.module";
 import { CustomUuidScalar } from "./general-parameter/scalars/buffer-scalar";
+import { Entity } from "./general-parameter/schemas/external/entity.schema";
 
 @Module({
    imports: [
-      GraphQLModule.forRoot<ApolloDriverConfig>({
-         driver: ApolloDriver,
+      GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+         driver: ApolloFederationDriver,
          playground: false,
          plugins: [ApolloServerPluginLandingPageLocalDefault()],
          autoSchemaFile: join(process.cwd(), "src/schema.gql"),
          resolvers: { BUFFER: CustomUuidScalar },
+         buildSchemaOptions : {
+            orphanedTypes : [Entity]
+         }
       }),
+      
       GeneralParameterModule,
    ],
    controllers: [],
---
+++ src/general-parameter/schemas/external/entity.schema.ts
@@ -0,0 +1,10 @@
+import { ObjectType, Field, Directive } from "@nestjs/graphql";
+import { CustomUuidScalar } from "src/general-parameter/scalars/buffer-scalar";
+
+@ObjectType()
+@Directive('@key(fields: "idEntity")')
+@Directive('@extends')
+export class Entity {
+  @Field(() => CustomUuidScalar)
+  idEntity: Buffer;
+}
