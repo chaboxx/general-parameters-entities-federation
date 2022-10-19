7c5505e76649480ae425d37422a993e534ecb52d
RODRIGO CUEVA PASTOR <chaboxx159@gmail.com>
2022-10-18 22:35:33 -0500

    feat : apollo federation config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,15 @@ import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { PrismaService } from "./prisma.service";
 import { join } from "path";
-import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from "@nestjs/apollo";
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 import { GeneralParameterModule } from "./general-parameter/general-parameter.module";
 import { CustomUuidScalar } from "./general-parameter/scalars/buffer-scalar";
 
 @Module({
    imports: [
-      GraphQLModule.forRoot<ApolloDriverConfig>({
-         driver: ApolloDriver,
+      GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+         driver: ApolloFederationDriver,
          playground: false,
          plugins: [ApolloServerPluginLandingPageLocalDefault()],
          autoSchemaFile: join(process.cwd(), "src/schema.gql"),
